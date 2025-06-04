a2409670c4102f57bb42db2946167f22763082c8
BDaws04 <ben.dawson@kcl.ac.uk>
2025-06-04 20:43:28 +0100

    add evaluator boilerplate code

---
+++ cpp_core/include/evaluator.h
@@ -0,0 +1,9 @@
+#ifndef EVALUATOR_H
+#define EVALUATOR_H
+
+#include "deck.h"
+
+
+
+
+#endif
--- cpp_core/src/evaluator.cpp
+++ cpp_core/src/evaluator.cpp
@@ -1,6 +1,18 @@
 #include <cstdint>
 
+#include "../include/evaluator.h"
 
-#define cs = uint16_t
-#define ss = uint64_t
+using cs = uint16_t;
+using ss = uint64_t;
+
+int evaluate_hand(const card hand[5]){
+    cs card_set = 0;
+    ss suit_set = 0;
+
+    encode_hand(hand, card_set, suit_set);
+}
+
+void encode_hand(const card hand[5], cs& card_set, ss& suit_set) {
+
+}
 
